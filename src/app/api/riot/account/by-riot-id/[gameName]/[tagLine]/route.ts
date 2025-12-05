9e2ccdbb45c3921f2e979f0197811915c5139969
Enguerrand <enguerrand.le-tourneur-hugon@oteria.fr>
2025-12-05 15:44:58 +0100

    Ajustement pour vercel

--- src/app/api/riot/account/by-riot-id/[gameName]/[tagLine]/route.ts
+++ src/app/api/riot/account/by-riot-id/[gameName]/[tagLine]/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { gameName: string; tagLine: string } }
 ) {
-  const { gameName, tagLine } = await params as { gameName: string; tagLine: string };
+  const { gameName, tagLine } = params;
   const RIOT_API_KEY = 'RGAPI-53a84f11-29d4-44e7-bba6-e00110be4058';
 
   if (!gameName || !tagLine) {
